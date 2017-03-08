4367010665604c4b4dba42c58b7f8e4efd6fd191
Meraj Ahmad Siddiqui <meraj.siddiqui@rankwatch.com>
2017-03-08 17:06:06 +0530

    First Commit

---
+++ examples/config/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include dirname(dirname(__DIR__)) . "/vendor/autoload.php";
+
+use IBMWatson\Speech\TextToSpeech;
+
+$text_speech_object = new TextToSpeech();
+
+?>
---
+++ src/IBMWatson/Speech/TextToSpeech.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package Text To Speech Using IBM Watson
+ * @author  Meraj Ahmad Siddiqui <merajsiddiqui@outlook.com>
+ */
+
+namespace IBMWatson\Speech;
+
+class TextToSpeech {
+
+	public $method_uri = "text-to-speech";
+}
