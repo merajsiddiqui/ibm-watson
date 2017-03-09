9733f19927fb173ab1da04b15d28af0c5305c12c
Meraj Ahmad Siddiqui <meraj.siddiqui@rankwatch.com>
2017-03-09 13:17:18 +0530

    More details added to Voice class

--- src/IBMWatson/Speech/Voice/Voice.php
+++ src/IBMWatson/Speech/Voice/Voice.php
@@ -8,15 +8,30 @@ namespace IBMWatson\Speech\Voice;
 use IBMWatson\Authenticate;
 
 class Voice {
-
+	/**
+	 * API version
+	 * @var string
+	 */
 	public $api_version = "v1";
-
+	/**
+	 * Method to be called
+	 * @var string
+	 */
 	public $api_method = "voices";
-
+	/**
+	 * The request handler
+	 * @var object
+	 */
 	protected $voice;
-
+	/**
+	 * Json data of available voice
+	 * @var string
+	 */
 	public $available_voices;
-
+	/**
+	 * Voice to be used for converting speech to text
+	 * @var array
+	 */
 	public $speech_voice;
 
 	public function __construct($configuration_file) {
@@ -32,7 +47,11 @@ class Voice {
 		$this->available_voices = $this->voice->authorize($this->api_version . "/" . $this->api_method)->getContents();
 		return json_decode($this->available_voices, true);
 	}
-
+	/**
+	 * This method filters the abailable voices on IBM watson platform
+	 * @param  array $filter_options filter options
+	 * @return array                 result of this method
+	 */
 	public function filterVoice($filter_options) {
 		$err_code = (!$filter_options) ? "no_filter" : "filter_not_found";
 		$filtered_voice = ["msg" => $this->errorMessage($err_code)];
@@ -50,11 +69,18 @@ class Voice {
 		}
 		return $filtered_voice;
 	}
-
-	public function setVoicePerson($voice_detail) {
+	/**
+	 * This method will set person voices
+	 * @param array $voice_detail The voice to be used
+	 */
+	public function setPersonVoice($voice_detail) {
 		$this->speech_voice = $voice_detail;
 	}
-
+	/**
+	 * This method returns apropriate error message for this class
+	 * @param  string $err_code error code to get the message
+	 * @return string           Error message for provided error code
+	 */
 	protected function errorMessage($err_code) {
 		$error_message = [
 			"no_filter" => "No Filter Options Were Provided",
